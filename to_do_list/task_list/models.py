0512e4a6d26ab77432b20c361ad595cea0b89e1b
Mihail Fomin PC <mihanaz098@gmail.com>
2026-02-02 18:19:54 +0300

    Add users

--- to_do_list/task_list/models.py
+++ to_do_list/task_list/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Note(models.Model):
@@ -6,6 +7,7 @@ class Note(models.Model):
     content = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
     completed = models.BooleanField(default=False, null=False, blank=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, default=1)
 
     def __str__(self):
         return self.title
