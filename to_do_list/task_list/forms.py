446f0021468de59a7f9473620e9830278f9eeeb3
Mihail Fomin PC <mihanaz098@gmail.com>
2026-02-06 18:07:22 +0300

    Little optimization

--- to_do_list/task_list/forms.py
+++ to_do_list/task_list/forms.py
@@ -3,9 +3,10 @@ from .models import Task
 
 
 class TaskForm(forms.ModelForm):
-    title = forms.CharField(max_length=200, required=True, label="Задача")
-    content = forms.CharField(widget=forms.Textarea, required=False, label="Пояснения к задаче")
 
     class Meta:
         model = Task  # Укажите модель
         fields = ['title', 'content']
+        widgets = {
+            'content': forms.Textarea(),
+        }
--- to_do_list/task_list/models.py
+++ to_do_list/task_list/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=200, null=False, blank=False)
-    content = models.TextField(null=True, blank=True)
+    title = models.CharField(max_length=200, null=False, blank=False, verbose_name="Задача")
+    content = models.TextField(null=True, blank=True, verbose_name="Пояснение к задаче")
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
     completed = models.BooleanField(default=False, null=False, blank=False)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False, blank=False, default=1)
